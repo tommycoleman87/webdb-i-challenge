b84983e777750951581f51defbd751cec1d84843
Tommy Coleman <tommiilee@gmail.com>
2019-09-09 16:17:47 -0400

    added dependancies and made a get request using sql/knex to accounts

--- server.js
+++ server.js
@@ -6,4 +6,12 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/accounts', (req, res) => {
+    db.select('*').from('accounts').then(result => {
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
 module.exports = server;
